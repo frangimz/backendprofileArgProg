0490a97e09a50364e6a26c626d0e4dc79b1836b8
frangimz <frangimz@gmail.com>
2022-07-20 17:20:14 -0300

    agregue cross origin en el PersonaController

--- src/main/java/com/bprofilefrangimz/bprofile/Controller/PersonaControler.java
+++ src/main/java/com/bprofilefrangimz/bprofile/Controller/PersonaControler.java
@@ -2,6 +2,7 @@ package com.bprofilefrangimz.bprofile.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import java.util.List;
 import com.bprofilefrangimz.bprofile.Entity.Persona;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaControler {
     @Autowired IPersonaService ipersonaService;
 
