9b986fef76755de097c87649288e00a1634d5979
frangimz <frangimz@gmail.com>
2022-07-23 18:51:40 -0300

    Corregi errores en CExperiencia

--- src/main/java/com/bprofilefrangimz/bprofile/Controller/CExperiencia.java
+++ src/main/java/com/bprofilefrangimz/bprofile/Controller/CExperiencia.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,6 +64,7 @@ public class CExperiencia {
         return new ResponseEntity(new Mensaje("Experiencia actualzada"), HttpStatus.OK);
     }
 
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?>_delete (@PathVariable("id") int id){
         //validamos si existe el id
         if(!sExperiencia.existsById(id))
@@ -70,5 +72,13 @@ public class CExperiencia {
         sExperiencia.delete(id);
         
         return new ResponseEntity(new Mensaje("Experiencia eliminada"),HttpStatus.OK);
+    }
+
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Experiencia> getById(@PathVariable("id") int id){
+        if(!sExperiencia.existsById(id))
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        Experiencia experiencia = sExperiencia.getOne(id).get();
+        return new ResponseEntity(experiencia, HttpStatus.OK);
     }
 }
