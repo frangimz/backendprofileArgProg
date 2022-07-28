b44040ec76dfd773f2392296d9ce9ce340e9a56b
frangimz <frangimz@gmail.com>
2022-07-28 03:07:46 -0300

    comente parte del codigo de BprofileApplication.java

--- src/main/java/com/bprofilefrangimz/bprofile/BprofileApplication.java
+++ src/main/java/com/bprofilefrangimz/bprofile/BprofileApplication.java
@@ -1,13 +1,12 @@
 package com.bprofilefrangimz.bprofile;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+//import java.util.Arrays;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class BprofileApplication {
@@ -17,7 +16,7 @@ public class BprofileApplication {
 	}
 
 
-	@Bean
+	/*@Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
@@ -31,6 +30,6 @@ public class BprofileApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
+	}*/
 
 }
