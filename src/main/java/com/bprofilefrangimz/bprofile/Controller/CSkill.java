9c84705cbbf04d2e7db8a40a2d27b3df830f1204
frangimz <frangimz@gmail.com>
2024-09-29 14:31:54 -0300

    agregando el cross origin dandole permiso a la pagina

--- src/main/java/com/bprofilefrangimz/bprofile/Controller/CSkill.java
+++ src/main/java/com/bprofilefrangimz/bprofile/Controller/CSkill.java
@@ -23,8 +23,8 @@ import com.bprofilefrangimz.bprofile.Service.SSkill;
 
 @RestController
 @RequestMapping("skill")
-//@CrossOrigin(origins = "https://hosting-myprofile.web.app")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://hosting-myprofile.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CSkill{
     @Autowired
     SSkill sSkill;
